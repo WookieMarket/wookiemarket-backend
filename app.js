b092a920c5cc583ee3fdaa87038efebb935b8957
Rober <rp_rober@hotmail.com>
2023-07-26 19:49:40 +0200

    cors

--- app.js
+++ app.js
@@ -14,11 +14,19 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+const cors = require("cors");
+
+// Configurar opciones CORS para permitir solicitudes desde localhost:3000
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // Algunos navegadores antiguos (IE11, varios SmartTVs) interpretarán 204 como 'sin contenido'
+};
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -69,6 +77,11 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Servidor backend funcionando en http://localhost:${port}`);
 });
 
 module.exports = app;

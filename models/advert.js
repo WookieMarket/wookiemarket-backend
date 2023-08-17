fefd8d2784eeef5cd3db6bc0572cf6de4ce700b1
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-08-17 08:51:30 +0200

    Merged main branch

--- models/advert.js
+++ models/advert.js
@@ -14,6 +14,42 @@ const advertSchema = mongoose.Schema({
   coin: { type: String, default: '€', index: true },
 });
 
+// Static methods filter adverts
+advertSchema.statics.list = function (filter, skip, limit, sort, fields) {
+  const query = Advert.find(filter);
+  query.skip(skip);
+  query.limit(limit);
+  query.sort(sort);
+  query.select(fields);
+  return query.exec();
+};
+
+/*advertSchema.statics.price = function (price) {
+    const newPrice = price.split('-');
+    const price1 = newPrice[0];
+    const price2 = newPrice[1];
+    const exactPrice = price;
+
+    if (price1 && price2) {
+        const query = Advert.find({ price: { $gte: price1, $lte: price2 } });
+        return query.exec();
+    } else if (price1) {
+        const query = Advert.find({ price: { $gte: price1 } });
+        return query.exec();
+    } else if (price2) {
+        const query = Advert.find({ price: { $lte: price2 } });
+        return query.exec();
+    } else if (exactPrice) {
+        const query = Advert.find({ price: { $eq: exactPrice } });
+        return query.exec();
+    }
+};*/
+
+advertSchema.statics.distinctCategories = function () {
+  const query = Advert.distinct('category');
+  return query.exec();
+};
+
 //Create model
 
 const Advert = mongoose.model('Advert', advertSchema);

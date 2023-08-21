ed461af1a96d9d605e35c11d7db513d527f0bf06
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-08-21 19:10:16 +0200

    First Test for AdvertDetail OK

--- routes/api/ads/_tests_/adverts.test.js
+++ routes/api/ads/_tests_/adverts.test.js
@@ -5,84 +5,19 @@ const Advert = require('../../../../models/Advert');
 
 describe('My API', () => {
   it('should make a call to find an ad by ID', async () => {
-    // Crear un stub para el método `findById` del modelo Advert
+    //Create a stub for the `findById` method of the Advert model
     const id = '1';
     const findByIdStub = sinon.stub(Advert, 'findById').resolves({ name: 'Anuncio 1' });
 
-    // Llamar a la función que quieres probar
     const result = await Advert.findById(id);
 
-    // Verificar que se hizo una llamada al método `findById` del modelo Advert
+    // Verify that a call was made to the `findById` method of the Advert model.
     expect(findByIdStub.calledOnce).to.be.true;
 
-    // Verificar que la función devolvió el resultado esperado
+    // Verify that the function returned the expected result
     expect(result).to.deep.equal({ name: 'Anuncio 1' });
 
-    // Restaurar el comportamiento original del método `findById`
+    // Restore original behavior of `findById` method
     findByIdStub.restore();
   });
 });
-
-
-
-
-
-/*const request = require('supertest');
-const app = require('../../../../app');
-
-//Mock advert simulations
-jest.mock('../_mocks_/Adverts');
-
-//Mock DB simulation
-jest.mock('mongoose', () => ({
-  connect: jest.fn(),
-  set: jest.fn(),
-  connection: {
-    on: jest.fn(),
-    once: jest.fn(),
-  },
-  Schema: jest.fn(() => ({
-    statics: {},
-  })),
-  model: jest.fn(() => ({})),
-}));
-
-//Othe mocks simulations
-jest.mock('http-errors', () => jest.fn());
-const mockExpress = () => {
-  const app = {
-    use: jest.fn(),
-    get: jest.fn(),
-    post: jest.fn(),
-    put: jest.fn(),
-    delete: jest.fn(),
-    listen: jest.fn(),
-  };
-  return app;
-};
-mockExpress.Router = () => ({
-  get: jest.fn(),
-  post: jest.fn(),
-  put: jest.fn(),
-  delete: jest.fn(),
-});
-
-jest.mock('express', () => mockExpress);
-
-jest.mock('path', () => ({
-  join: jest.fn(),
-}));
-jest.mock('cookie-parser', () => jest.fn());
-jest.mock('morgan', () => jest.fn());
-jest.mock('dotenv', () => ({
-  config: jest.fn(),
-}));
-
-//Testing
-describe('GET /api/ads/adverts/:id', () => {
-  it('should return the details of an ad', async () => {
-    const res = await request(app).get('/anuncio/1').expect(200);
-    expect(res.body).toHaveProperty('title');
-    expect(res.body).toHaveProperty('description');
-  });
-});*/
